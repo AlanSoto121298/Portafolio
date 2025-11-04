3e963bb5f57ac192c7ea232ac15b0468b9b48356
Alan Soto <alansoto121298@gmail.com>
2025-11-04 11:36:20 -0600

    cambio

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 //llamar el ID uno para redireccionamiento a about me 
 document.addEventListener("DOMContentLoaded", () => {
-    const divUno = document.getElementById("dos");
+    const divUno = document.getElementById("aboutme");
 
     divUno.addEventListener("click", () => {
         window.location.href = "aboutme.html";
     });
 });
+
+//llamar el ID uno para redireccionamiento a Projects
+document.addEventListener("DOMContentLoaded", () => {
+    const divDos = document.getElementById("projects");
+
+    divDos.addEventListener("click", () => {
+        window.location.href = "projects.html";
+    });
+});
+
+//llamar el ID para el formulario
+document.addEventListener("DOMContentLoaded",()=>{
+    const divTres = document.getElementById("contact");
+
+    divTres.addEventListener("click",() =>{
+        window.location.href = "contact.html";
+    });
+});
+
