72b42137766b3a764aac40c5aec733df39131693
Alan Soto <alansoto121298@gmail.com>
2025-12-17 15:03:01 -0600

    contact y script new

---
+++ scripts/scriptContact.js
@@ -0,0 +1,8 @@
+// BOTONES PARA REGRESAR A INDEX HTML
+document.addEventListener("DOMContentLoaded",()=>{
+    const boton = document.getElementById("btn-contact");
+
+    boton.addEventListener("click",() =>{
+        window.location.href = "index.html";
+    });
+});
