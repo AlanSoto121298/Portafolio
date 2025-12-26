a7ebfaaa297c9b741264a3b371e6bc5fb09a711a
Alan Soto <alansoto121298@gmail.com>
2025-12-26 12:07:57 -0600

    projects

--- scripts/script.js
+++ scripts/script.js
@@ -44,16 +44,6 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 
 
-// BOTONES PARA REGRESAR A INDEX HTML
-document.addEventListener("DOMContentLoaded",()=>{
-    const boton = document.getElementById("btn-aboutme");
-
-    boton.addEventListener("click",() =>{
-        window.location.href = "index.html";
-    });
-});
-
-
 
 
 
