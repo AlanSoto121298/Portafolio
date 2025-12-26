dba881621d1e6b2cc3240d4e3c1815be8dabf135
Alan Soto <alansoto121298@gmail.com>
2025-12-26 13:03:08 -0600

    e

---
+++ scripts/scriptinndaka.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const boton = document.getElementById("btn-inndaka");
+
+    boton.addEventListener("click",() =>{
+     window.location.href = "../projects.html";
+
+
+    });
+});
