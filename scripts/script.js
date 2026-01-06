d30c7b25d701185af9d3d16748f906cdfac41d08
Alan Soto <alansoto121298@gmail.com>
2026-01-06 12:24:30 -0600

    fondos con un array

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,28 @@
+//CAMBIAR FONDO 
+ const fondos = [
+    'linear-gradient(135deg, #000000 0%, #000000 80%, #002200 90%, #004400 100%)',
+    'linear-gradient(135deg, #ff0000 0%, #ff5500 50%, #ffaa00 100%)',
+    'linear-gradient(135deg, #000044 0%, #004466 50%, #00aaaa 100%)',
+    'linear-gradient(135deg, #222222 0%, #555555 50%, #888888 100%)'
+  ];
+
+  let indice = 0; // empieza con el primer fondo
+
+  const boton = document.getElementById('cambiarFondo');
+
+  boton.addEventListener('click', () => {
+    // cambiar el índice al siguiente fondo
+    indice++;
+    if (indice >= fondos.length) {
+      indice = 0; // volver al primero si llegamos al final
+    }
+
+    // aplicar el fondo al body
+    document.body.style.background = fondos[indice];
+  });
+
+
+
 //CAMBIAMOS EL TITULO COMO TIPO SALUDO Y REGRESAMOS AL MISMO CON SET TIME OUT
 
 const nombreElemento = document.querySelector("#nombre");
